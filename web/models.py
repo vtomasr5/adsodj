e2dba3e5407ffd3edbe8890a42b3bcc769235aab
Vicenç Juan Tomàs <vtomasr5@gmail.com>
2011-05-12 17:28:05 +0200

    changes at model

--- web/models.py
+++ web/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Paginas(models.Model):
-    title = models.CharField(max_length=400)
-    link = models.CharField(max_length=400)
+    title = models.CharField(max_length=400, unique=True)
+    link = models.CharField(max_length=400, unique=True)
 
     def __unicode__(self):
         return self.title
